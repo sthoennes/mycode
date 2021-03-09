4924ded522473f94b9cfe4891897f0687b3b1b72
sthoennes <susan.thoennes@usbank.com>
2021-03-09 18:23:51 +0000

    your commit message

---
+++ iftest/condition03.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+hostname = input("What value should we set for hostname?")
+## Notice how the next line has changed
+## here we use the str.lower() method to return a lowercase string
+if hostname.lower() == "mtg":
+    print("The hostname was found to be mtg")
+    # Tell user it is expected config
+    print("hostname matched expected config")
+# End of script
+print("Exiting script now")
