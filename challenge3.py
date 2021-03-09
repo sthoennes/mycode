896974ae60347ef0ddc8a1f2895fea206b9f4c62
sthoennes <susan.thoennes@usbank.com>
2021-03-09 16:10:43 +0000

    test

---
+++ challenge3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+
+trial= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
+
+nightmare= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}},"banana": 15, "d": "nothing"}]
+
+# Print "My eyes! The goggles do nothing!
+print(f"My {challenge[2][1]}! The {challenge[2][0]} do {challenge[3]}!")
+
+#print(f"My {trial[2]['goggles']}! The {trial[2]['eyes']} do {trial[3]}!")
+print(f"My {trial[2]['goggles']}! The {trial[2]['eyes']} do {trial[3]}!")
+
+#
+#print(f"{nightmare[0]['user']['name']['first']}! {nightmare[0]['kumquat']} do {nightmare['d']}!")
+print(f"My {nightmare[0]['user']['name']['first']}! The {nightmare[0]['kumquat']} do {nightmare[0]['d']}!")
