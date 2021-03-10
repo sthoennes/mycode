095d5aa63a56455706f5160fa271afb4db8a5f88
sthoennes <susan.thoennes@usbank.com>
2021-03-10 17:08:06 +0000

    Looping techniques

--- introfor/forloop3.py
+++ introfor/forloop3.py
@@ -2,23 +2,23 @@
 # create the list called farms
 farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
          {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
-         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]},
+         {"name": "CHAD"]}
 item = 0
 #val = '0'
 # loop across the list farmss
-for item in farms[item]:
-    print(item)
-    for val in item[val]:
-        print(val)
-#for key in farms:
-#    print(type(key))
- #   if type(key) == "<class 'dict'>":
-  #      print(key.value())
-   #     print('Hi Sue')
-    #print(key)
-print("\nOur loop has ended.")  # when the loop ends print this
-
-#for key,value in farms.items():
+# dont repeat code, dont make inflexible, what if more farms added or removed, what if name or agg doesnt have value?
+# define function for repeatable code
 
 
-    
+def farm(firefly):
+# let firefly equal the farm dic being passed into this function
+    print(f"Resourece is {firefly.get('name','UNAMED FARM')}:")
+    for animals in firefly.get('agriculture'):
+        print(animals)
+    print()
+
+for x in farms:
+# i want to talke to each dict and i want to get name an aggriculture from both dicts
+    farm(x)
+
---
+++ introfor/forloop4.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# create a star pyramid
+starlist= ["* ","* * ","* * * ","* * * * ","* * * * * ","* * * * ","* * * ","* *" ,"*"]
+for x in starlist:
+    print(x)  # each time through the loop print value of xis all I have so far - 
+star = "*"
+
+#print()
+#print(star,"\n")
+#print(star,star,"\n")
+#print(star,star,star,"\n")
+#print(star,star,"\n")
+#print(star,"\n")
+input("how big do you want pyramid to go, enter odd number up to 15",size)
+while size < 15
+    size += 1
+    starlist.extend("*")
+starlist2=star.append(star)
+for p le 5 :
+    p += 1
+    y=star.append(star)
+    print(y)
+
